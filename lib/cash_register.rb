1fcde205b189205b2f74dc3a6fcc2a1d69b117d1
Marsha Annon <marshaannon@gmail.com>
2020-03-15 21:02:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,9 +25,8 @@ class CashRegister
       "After the discount, the total comes to $#{self.total.to_i}."
   end
 
-  def apply_discount
-    self.total == 0 ? "There is no discount to apply." : "After the discount, the total comes to $#{self.total = self.total * 8 / 10 }."
-  end
+  
+
   def void_last_transaction
     self.total -= self.last_transaction[1] * self.last_transaction[2]
     self.last_transaction[2].times do
