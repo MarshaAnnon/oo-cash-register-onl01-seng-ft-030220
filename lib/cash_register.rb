12c4b9fd9ffd80321ea8c3b612e62d20882e9c1a
Marsha Annon <marshaannon@gmail.com>
2020-03-15 20:33:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   def add_item(title, price, quantity=1)
     @total += title && price * quantity
     @total
-    @items << item
+    @items << title
   end
 
   def apply_discount
