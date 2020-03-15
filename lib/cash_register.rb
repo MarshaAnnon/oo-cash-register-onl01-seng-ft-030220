e3a9c0a4ddbb207c378de6cc881d017d0994bcb2
Marsha Annon <marshaannon@gmail.com>
2020-03-15 19:06:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ class CashRegister
     @discount / 100.to_f * @total
     @total -= @discount * 10
       if @discount
-        "After the discount, the total comes to #{@total}."
+        "After the discount, the total comes to #{@total}.to_i."
       else
         "There is no discount to apply."
     end
