4f928838a30d766d0ea4a458ac3d6f2f0b175161
Marsha Annon <marshaannon@gmail.com>
2020-03-15 20:46:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity=1)
-    @total += title && price * quantity
+    @total += title && price * quantity +=1
     @total
     @items << title
   end
